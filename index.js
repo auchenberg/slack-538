76b1cea91009684af8e2107258f3d45bd0237c4a
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-10-08 10:33:06 -0500

    MOre

--- index.js
+++ index.js
@@ -48,8 +48,40 @@ controller.setupWebserver(process.env.PORT,function(err,webserver) {
   });
 });
 
-controller.on('538',function(bot,message) {
-    console.log('538', bot,message)
-    // reply to slash command
-    bot.replyPublic(message,'Everyone can see the results of this slash command');
+controller.on('slash_command', function (slashCommand, message) {
+
+    switch (message.command) {
+        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+            // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
+            // Otherwise just echo back to them what they sent us.
+
+            // but first, let's make sure the token matches!
+            //if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+
+            // if no text was supplied, treat it as a help command
+            if (message.text === "" || message.text === "help") {
+                slashCommand.replyPrivate(message,
+                    "I echo back what you tell me. " +
+                    "Try typing `/echo hello` to see.");
+                return;
+            }
+
+            // If we made it here, just echo what the user typed back at them
+            //TODO You do it!
+            slashCommand.replyPublic(message, "1", function() {
+                slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
+            });
+
+            break;
+
+        case "/538":
+            slashCommand.replyPublic(message, "538 graph coming here");
+            break;
+            
+        default:
+            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+
+    }
+
 })
+;
