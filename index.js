aacfa6351ba48c3ed6a58f7330677fe45a587f85
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-10-08 09:53:10 -0500

    more

--- index.js
+++ index.js
@@ -22,26 +22,65 @@
 //   file.write(data.toString('binary'), 'binary');
 // });
 
-var SlackBot = require('slackbots');
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           ______     ______     ______   __  __     __     ______
+          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
+          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
+           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
+            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+            
+This is a sample Slack Button application that provides a custom
+Slash command.
+This bot demonstrates many of the core features of Botkit:
+*
+* Authenticate users with Slack using OAuth
+* Receive messages using the slash_command event
+* Reply to Slash command both publicly and privately
+# RUN THE BOT:
+  Create a Slack app. Make sure to configure at least one Slash command!
+    -> https://api.slack.com/applications/new
+  Run your bot from the command line:
+    clientId=<my client id> clientSecret=<my client secret> port=3000 node bot.js
+    Note: you can test your oauth authentication locally, but to use Slash commands
+    in Slack, the app must be hosted at a publicly reachable IP or host.
+# EXTEND THE BOT:
+  Botkit has many features for building cool and useful bots!
+  Read all about it here:
+    -> http://howdy.ai/botkit
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var Botkit = require('botkit');
 
-// create a bot
-var bot = new SlackBot({
-    token: 'xoxb-89062814325-30k102AQ4vRkSC7p4fAxlpZ4', // Add a bot https://my.slack.com/services/new/bot and put the token 
-    name: '538'
+if (!process.env.clientId || !process.env.clientSecret || !process.env.port) {
+  console.log('Error: Specify clientId clientSecret and port in environment');
+  process.exit(1);
+}
+
+var controller = Botkit.slackbot({
+  json_file_store: './db_slackbutton_slashcommand/',
+}).configureSlackApp({
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    scopes: ['commands'],
+  });
+
+
+controller.setupWebserver(process.env.port,function(err,webserver) {
+
+  controller.createWebhookEndpoints(controller.webserver);
+
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+    } else {
+      res.send('Success!');
+    }
+  });
 });
 
-bot.on('start', function() {
-    // more information about additional params https://api.slack.com/methods/chat.postMessage
-    var params = {
-        icon_emoji: ':flag-us:'
-    };
 
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    bot.postMessageToChannel('dapol', 'Hello from 538', params);
+controller.on('538',function(bot,message) {
 
-});
+  bot.replyPublic(message,'<@' + message.user + '> is cool!');
+  bot.replyPrivate(message,'*nudge nudge wink wink*');
 
-bot.on('message', function(data) {
-    // all ingoing events https://api.slack.com/rtm
-    console.log(data);
 });
