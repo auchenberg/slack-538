f711f004d4ec2537f9f3e45fb6a07e1610dc3f0a
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-10-12 14:33:45 -0700

    StandardJS

--- worker.js
+++ worker.js
@@ -4,9 +4,8 @@ var fivethirtyeight = require('./fivethirtyeight')
 
 fivethirtyeight.getForecast().then(forecastImageUrl => {
   console.log('worker.getForecast.success', forecastImageUrl)
-  process.exit();
+  process.exit()
 }).catch(err => {
   console.error('worker.getForecast.failed', err)
-  process.exit();
+  process.exit()
 })
-
