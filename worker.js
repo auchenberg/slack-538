c378f503695e91a4d225f3b20c15330091432e66
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-10-12 13:33:49 -0700

    Update worker

--- worker.js
+++ worker.js
@@ -4,7 +4,9 @@ var fivethirtyeight = require('./fivethirtyeight')
 
 fivethirtyeight.getForecast().then(forecastImageUrl => {
   console.log('worker.getForecast.success', forecastImageUrl)
+  process.exit();
 }).catch(err => {
-  console.error('worker.getForecast.success', err)
+  console.error('worker.getForecast.failed', err)
+  process.exit();
 })
 
