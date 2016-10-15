76baf59e42bb855535d9aafa351983937b6c7d51
Kenneth Auchenberg <kenneth@auchenberg.dk>
2016-10-14 22:38:43 -0700

    use node-scheduler to run cron job

--- worker.js
+++ worker.js
@@ -1,11 +1,20 @@
 'use strict'
 
 var fivethirtyeight = require('./fivethirtyeight')
+var schedule = require('node-schedule')
 
-fivethirtyeight.getForecast().then(forecastImageUrl => {
-  console.log('worker.getForecast.success', forecastImageUrl)
-  process.exit()
-}).catch(err => {
-  console.error('worker.getForecast.failed', err)
-  process.exit()
+var fetch = function() {
+
+  fivethirtyeight.getForecast().then(forecastImageUrl => {
+    console.log('worker.getForecast.success', forecastImageUrl)
+    process.exit()
+  }).catch(err => {
+    console.error('worker.getForecast.failed', err)
+    process.exit()
+  })
+
+}
+
+schedule.scheduleJob('* /30 * * *', function(){
+  fetch()
 })
